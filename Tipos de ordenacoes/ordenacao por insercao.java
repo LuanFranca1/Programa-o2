a8b202183070ef2159c1c91151af5c569c469998
LuanFranca1 <99998698+LuanFranca1@users.noreply.github.com>
2022-03-21 10:48:14 -0300

    Create ordenacao por insercao.java

---
+++ Tipos de ordenacoes/ordenacao por insercao.java
@@ -0,0 +1,30 @@
+
+public class Ordencao {
+
+    public static void main(String[] args) {
+
+        int[] vet = {1, 6, 8, 4, 3, 9, 11, 10};
+        
+       vet=insercao(vet);
+        for (int i = 0; i < vet.length; i++) {
+            System.out.println(vet[i]);
+        }
+    }
+    public static int[] insercao(int[] vet) {
+        int atual;
+        int i, j;
+       
+        for (i = 2; i < vet.length; i++) {
+            atual = vet[i];
+            j = i - 1;
+            while ((j > 0) && (vet[j] > atual)) {
+                vet[j + 1] = vet[j];
+                j = j - 1;
+            }
+            vet[j + 1] = atual;
+            //System.out.println(atual);
+        }
+        return vet;
+    }
+}
+//       while ((j >= 0) && (vet[j] < atual)) { (decrescendo);
