863cbc2450dc33136788d291eba02fe08bccb199
LuanFranca1 <99998698+LuanFranca1@users.noreply.github.com>
2022-03-24 10:36:45 -0300

    Create ordenacao selecionada.java

---
+++ Tipos de ordenacoes/ordenacao selecionada.java
@@ -0,0 +1,30 @@
+public class Selecao {
+
+    public static void main(String[] args) {
+
+        double[] vet = {-2, 5, 7, -1, -6, 10};
+        vet=ordenacaoS(vet);
+        for (int i = 0; i < vet.length; i++) {
+            System.out.println(vet[i]);
+      }
+    }
+    public static double[] ordenacaoS(double[] vet) {
+        int i, j;
+        int max = 0;
+        double aux;
+        for (i = vet.length; i > 0; i--) {
+            max = i;
+            for (j = 1; j < i - 1; i++) {
+                if(vet[j] > vet[max]){
+                    max=j;
+                }
+                vet[max] = vet[i];
+            }
+        }
+        aux = vet[max];
+        vet[max] = vet[1];
+        vet[i] = aux;
+        return vet;
+    }
+
+}
