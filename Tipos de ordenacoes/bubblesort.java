ad16df1d1883b7a1af26763bd39b248fa01e48c2
LuanFranca1 <99998698+LuanFranca1@users.noreply.github.com>
2022-03-28 10:07:03 -0300

    Create bubblesort.java

---
+++ Tipos de ordenacoes/bubblesort.java
@@ -0,0 +1,27 @@
+public class Bubblesort {
+    
+    public static void main(String[]args){ 
+        
+        double [] vet = {-2,-4,10,5,2,3};
+        double [] result = new double[vet.length];
+        
+        vet = bubble(vet);
+        for (int i = 0; i < vet.length; i++) {
+            System.out.println(vet[i]);
+        }
+    }
+    public static double[] bubble(double [] vet){
+        int i,j,k;
+        double temp;
+        for (j = 0; j < vet.length; j++) {
+            for (i = 0; i < vet.length -1 ; i++) {
+                if(vet[i] > vet[i+1]){
+                    temp=vet[i];
+                    vet[i] = vet[i+1];
+                    vet[i+1] = temp;
+                }
+            }
+        }
+        return vet;
+    }
+}
