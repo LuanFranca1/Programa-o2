0444c1ed7ab686c672cf016884dea6a06671b3ac
LuanFranca1 <99998698+LuanFranca1@users.noreply.github.com>
2022-03-28 10:18:25 -0300

    Update bubblesort.java

--- Tipos de ordenacoes/bubblesort.java
+++ Tipos de ordenacoes/bubblesort.java
@@ -15,7 +15,7 @@ public class Bubblesort {
         double temp;
         for (j = 0; j < vet.length; j++) {
             for (i = 0; i < vet.length -1 ; i++) {
-                if(vet[i] > vet[i+1]){
+                if(vet[i] > vet[i+1]){ //ordem decrescente é só alterar o sinal
                     temp=vet[i];
                     vet[i] = vet[i+1];
                     vet[i+1] = temp;
