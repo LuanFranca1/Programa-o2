7d3880094547f5e544265dd9c348567300f3266f
LuanFranca1 <99998698+LuanFranca1@users.noreply.github.com>
2022-04-01 10:02:01 -0300

    Update PrimeiraQuestao.java

--- Lista2/PrimeiraQuestao.java
+++ Lista2/PrimeiraQuestao.java
@@ -1,1 +1,25 @@
+package javaapplication1;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        int[] vet = {1, 2, 3, 10, 8, 9, 6, 7};
+        int buscando=10;
+        int n;
+        System.out.println(buscaLinear(vet,buscando));
+    }
+     public static int buscaLinear(int[] vet, int buscando) {
+        int i = 0;
+        while (i <= vet.length) {
+            if (i == vet.length) {
+            } else if (vet[i] == buscando) {
+                return i;
+
+            }
+            i++;
+        }
+        return -1;
+    }
+
+}
 
