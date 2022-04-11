90a3d08d50955b5ee9a41133c48d9a748ab19a0d
LuanFranca1 <99998698+LuanFranca1@users.noreply.github.com>
2022-04-11 09:41:42 -0300

    Update ordenacao selecionada.java

--- Tipos de ordenacoes/ordenacao selecionada.java
+++ Tipos de ordenacoes/ordenacao selecionada.java
@@ -28,3 +28,39 @@ public class Selecao {
     }
 
 }
+
+/*public class Teste {
+
+    public static void main(String[] args) {
+        int[][] matrizOrdenada = {{9, 5, 2}, {4, 7, 6}, {1, 8, 3}};
+        int coluna = 2;
+        int[][] ordenaMatrizC = ordenaMatrizC(matrizOrdenada, coluna);
+        for (int i = 0; i < matrizOrdenada.length; i++) {
+            for (int j = 0; j < matrizOrdenada[i].length; j++) {
+                System.out.println(ordenaMatrizC[i][j]);
+            }
+        }
+    }
+    public static int[][] ordenaMatrizC(int[][] matriz, int coluna) {
+        // int[][] matrizOrdenada = {{9, 5, 2}, {4, 7, 6}, {1, 8, 3}};
+        int max;
+        int aux;
+        for (int i = matriz.length - 1; i > 0; i--) {
+            max = i;
+            for (int j = 0; j < i; j++) {
+                if (matriz[j][coluna] > matriz[max][coluna]) {
+                    max = j;
+                }
+                matriz[i][coluna] =  matriz[max][coluna];
+            }
+            for (int k = 0; k < matriz[i].length; k++) {
+                aux = matriz[max][k];
+                matriz[max][k] = matriz[i][k];//trocar essa 
+                matriz[i][k] = aux;
+            }
+        }
+        return matriz;
+    }
+
+}
+*/
