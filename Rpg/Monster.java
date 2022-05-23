d87a26a7463a0f26849b051eaef933f5db938690
LuanFranca1 <99998698+LuanFranca1@users.noreply.github.com>
2022-05-23 16:06:32 -0300

    Create Monster.java

---
+++ Rpg/Monster.java
@@ -0,0 +1,20 @@
+package rpg;
+
+public class Monster extends Character {
+
+    public Monster(int hp, int ataque, String status, int poderMagic, String atacar, int porcao) {
+        this.hp = 24;
+        this.ataque = 4;
+        this.status = "normal";
+        this.poderMagic = 6;
+    }
+    public void atacar(){
+
+    }
+    public void recuperarHP(){
+
+    }
+    public void endurecer() {
+
+    }
+}
