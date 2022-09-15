4fc76e7729a617f2b86a70379d3080a213178463
LuanFranca1 <99998698+LuanFranca1@users.noreply.github.com>
2022-09-15 15:29:13 -0300

    Update Monster.java

--- Rpg/Monster.java
+++ Rpg/Monster.java
@@ -2,19 +2,40 @@ package rpg;
 
 public class Monster extends Character {
 
-    public Monster(int hp, int ataque, String status, int poderMagic, String atacar, int porcao) {
-        this.hp = 24;
-        this.ataque = 4;
-        this.status = "normal";
-        this.poderMagic = 6;
+    public Monster(String nome) {
+        super(nome, 24, 4, "normal", 6);
     }
-    public void atacar(){
-
+    public void atacar(Warrior jogador) {
+        int hpJogador =  jogador.getHp();
+        if (getAtacar()  >= 0) {
+            jogador.setHp( hpJogador - this.getAtacar());
+        } else {
+            hpJogador = 0;
+            jogador.setHp(hpJogador);
+        }
     }
-    public void recuperarHP(){
-
+    public void recuperarHP() {
+        int hpMonstro =  super.getHp();
+        if ((hpMonstro + 2) > 24){
+            hpMonstro = 24;
+    }else{
+        hpMonstro = hpMonstro + 2;
+        super.setPoderMagico(super.getPoderMagico() - 1);
+        super.setHp(hpMonstro);
     }
-    public void endurecer() {
-
+}
+    public void endurecer(Warrior jogador, int turno) {
+        if (turno == 1) {
+            int poderM = super.getPoderMagico();
+            if (poderM >= 0){
+                poderM = poderM - 2;
+                super.setStatus("Fortificado");
+                super.setAtacar(super.getAtacar()+2);
+                super.setPoderMagico(poderM);
+            }
+        }else if (turno == 4) {
+            super.setStatus("Normal");
+            super.setPoderMagico(getPoderMagico() -2);
+        }
     }
 }
